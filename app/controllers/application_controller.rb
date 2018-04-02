886b21eb11430e70de466039d1598fb458fb43a3
Aaron Meaney <aaronmeaney@hotmail.com>
2018-04-03 00:21:21 +0100

    Setup navar, footer, background and color theme for main application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  # Redirect to login if the user is not authenticated
+  def authenticate_user!
+    if doctor_signed_in?
+      super
+    else
+      redirect_to new_doctor_session_path
+    end
+  end
 end
--- app/controllers/medical_facility_controller.rb
+++ app/controllers/medical_facility_controller.rb
@@ -1,4 +1,6 @@
 class MedicalFacilityController < ApplicationController
+	before_action :authenticate_doctor!
+
 	def index
 	end
 end
